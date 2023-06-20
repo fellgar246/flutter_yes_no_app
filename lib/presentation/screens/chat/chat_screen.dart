a7251b9358345e205cb7cd576c8cd1b218c881af
fellgar246 <fellgar246@gmail.com>
2023-06-20 11:44:11 -0600

    field text

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:flutter_yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:flutter_yes_no_app/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -39,7 +40,8 @@ class _ChatView extends StatelessWidget {
                 ? const HerMessageBubble()
                 : const MyMessageBubble();
             })),
-            Text('Mundo')
+            // Caja de texto de mensajes
+            const MessageFieldBox(),
           ],
         ),
       ),
