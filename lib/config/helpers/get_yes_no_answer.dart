f5e3d37f47df93875c6277405cba9350d8d56f74
fellgar246 <fellgar246@gmail.com>
2023-07-01 14:05:19 -0600

    model answer

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -8,12 +8,9 @@ class GetYesNoAnswer {
   Future<Message> getAnswer() async {
     final response = await _dio.get('https://yesno.wtf/api');
 
-    final yesNoModel = YesNoModel.fromJson(response.data);
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
 
-    return Message(
-      text: yesNoModel.answer,
-      fromWho: FromWho.you,
-      imageUrl: yesNoModel.image,
-    );
+    return yesNoModel.toMessageEntity();
   }
+
 }
--- lib/infrastructure/models/yes_no_model.dart
+++ lib/infrastructure/models/yes_no_model.dart
@@ -1,15 +1,31 @@
+import '../../domain/entities/message.dart';
+
 class YesNoModel {
-  String answer;
-  bool forced;
-  String image;
+  final String answer;
+  final bool forced;
+  final String image;
 
-  YesNoModel({ required this.answer, required this.forced, required this.image});
+  YesNoModel({
+      required this.answer,
+      required this.forced,
+      required this.image,
+  });
 
-  factory YesNoModel.fromJson(Map<String, dynamic> json) {
-    return YesNoModel(
-      answer: json['answer'],
-      forced: json['forced'],
-      image: json['image'],
-    );
-  }
+  factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
+      answer: json["answer"],
+      forced: json["forced"],
+      image: json["image"],
+  );
+
+  Map<String, dynamic> toJson() => {
+      "answer": answer,
+      "forced": forced,
+      "image": image,
+  };
+
+  Message toMessageEntity() => Message(
+    text: answer == 'yes' ? 'Sí' : 'No',
+    fromWho: FromWho.you,
+    imageUrl: image,
+  );
 }
